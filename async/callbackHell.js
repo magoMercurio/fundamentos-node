070a9c88b63a9965dc3697a02126bea1b45d2e7c
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-12 12:01:10 -0500

    Update callbackHell.js

--- async/callbackHell.js
+++ async/callbackHell.js
@@ -9,7 +9,7 @@ function hablar(callbackHablar) {
   setTimeout (function() {
     console.log('Bla bla bla bla...');
     callbackHablar();
-  })
+  }, 1000);
 }
 
 function adios(nombre, otroCallback) {
@@ -17,17 +17,32 @@ function adios(nombre, otroCallback) {
     console.log('adios', nombre);
     otroCallback();
   }, 1500);
+}
 
+function conversacion(nombre, veces, callBack) {
+  if (veces > 0) {
+    hablar(function () {
+      conversacion(nombre, --veces, callBack);
+    })
+  } else {
+    adios(nombre, callBack);
+  }
 }
 
 // --
 
 console.log('iniciando Proceso');
-hola('Carlos', function () {
+hola('carlos', function () {
+  conversacion('carlos', 3, function() {
+    console.log('terminando proceso');
+  });
+});
+
+/* hola('Carlos', function () {
 
   hablar(function () {
     adios('Carlos', function() {
       console.log('terminando Proceso');
     });
   });
-});
+}); */
