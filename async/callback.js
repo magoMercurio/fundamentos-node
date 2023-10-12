916afc1a305263695c8ae45ccd877054a2e6d27e
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-12 11:33:03 -0500

    Update callback.js

--- async/callback.js
+++ async/callback.js
@@ -1,7 +1,22 @@
-function soyAsincrona() {
-    console.log("Soy asincrona");
+function hola(nombre, miCallback) {
+  setTimeout(function () {
+    console.log('hola,' + nombre);
+    miCallback();    
+  }, 1000);
 }
 
-soyAsincrona();
+function adios(nombre, otroCallback) {
+  setTimeout(function () {
+    console.log('adios', nombre);
+    otroCallback();
+  }, 1500);
 
-console.log('terminando Proceso');
+}
+
+console.log('iniciando Proceso');
+hola('Carlos', function () {
+  adios('Carlos', function() {
+    console.log('terminando Proceso');
+  })
+
+});
