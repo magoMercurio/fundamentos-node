6ab54a422f887f66140d6d4472b944f55a71d506
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-13 09:53:13 -0500

    Update promises.js

--- async/promises.js
+++ async/promises.js
@@ -1,20 +1,47 @@
-function hola(nombre, miCallback) {
-  setTimeout(function () {
-    console.log('hola,' + nombre);
-    miCallback();    
-  }, 1000);
+function hola(nombre) {
+  return new Promise(function(resolve, reject) {
+
+    setTimeout(function () {
+      console.log('hola,' + nombre);
+      resolve(nombre);    
+    }, 1000);
+  });
 }
 
-function hablar(callbackHablar) {
-  setTimeout (function() {
-    console.log('Bla bla bla bla...');
-    callbackHablar();
-  }, 1000);
+function hablar(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout (function() {
+      console.log('Bla bla bla bla...');
+      resolve(nombre);
+      reject('Hay un error');
+    }, 1000);
+  });
 }
 
-function adios(nombre, otroCallback) {
-  setTimeout(function () {
-    console.log('adios', nombre);
-    otroCallback();
-  }, 1500);
+function adios(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log('adios', nombre);
+      resolve(nombre);
+    }, 1000);
+  });
 }
+
+// ---
+
+console.log('Iniciando el proceso...');
+
+
+hola('Raul')
+  .then(hablar)
+  .then(hablar)
+  .then(hablar)
+  .then(adios)
+  .then((nombre) => {
+    console.log('terminando el proceso');
+  })
+  .catch(error => {
+    console.error('Ha habido un error');
+    console.error(error);
+  
+  })
