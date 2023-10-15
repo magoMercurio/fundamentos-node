21c1f48f676c40be5905a48d495a09631f1c277d
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-15 11:20:02 -0500

    Update errores.js

--- modulos/errores.js
+++ modulos/errores.js
@@ -1,5 +1,29 @@
+function otrafuncion() {
+  return serompe();
+}
+
 function serompe() {
   return 3 + z;
 }
 
-serompe();
+function seRompeAsincrona() {
+  setTimeout(function() {
+    try {
+      return 3 + z;
+    } catch (err) {
+      console.error("Error en mi funcion asincrona");
+      console.error(err);
+    }
+}, 1000)
+}
+
+try {
+  // otrafuncion();
+  seRompeAsincrona(function (err) {
+    console.log(err.message);
+  });
+} catch(err) {
+  console.log(err);
+}
+
+console.log("Esto esta al final");
