01661b0f05d778235343596b917653a861cf9b59
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-16 09:19:02 -0500

    Update procesoHijo.js

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,26 @@
+const { exec, spawn } = require('child_process');
+const { stdout } = require('process');
+
+/* exec('node modulos/consola.js', (err, stdout, sterr) => {
+  if (err) {
+    console.log(err);
+    return false;
+  }
+
+  console.log(stdout);
+}) */
+
+let proceso = spawn('ls', ['-la']);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function (dato) {
+  console.log('Esta muerto?');
+  console.log(proceso.killed);
+  console.log(dato.toString());
+})
+
+proceso.on('exit', function() {
+  console.log('El proceso termino');
+})
