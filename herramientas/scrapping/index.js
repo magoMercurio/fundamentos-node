a081da1354b958e7da69d5e540fa828c3f013e46
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-17 12:39:06 -0500

    update

--- herramientas/scrapping/index.js
+++ herramientas/scrapping/index.js
@@ -1,5 +1,20 @@
-const puppeeteer = require('puppeteer')
+const pup = require('puppeteer');
 
-(async () => {
-  //codigo
-})
+(async ()=>{
+    console.log('lanzamos navegador');
+    //const browser = await pup.launch();
+    const browser = await pup.launch({headless:false});
+    const page = await browser.newPage();
+    await page.goto('https://es.wikipedia.org/wiki/Node.js')
+    // acciones
+    var titulo1 = await page.evaluate(()=>{
+        const h1 = document.querySelector('h1');
+        console.log(h1.innerText);
+        return h1.innerText;
+    })
+    //imprimimos
+    console.log(titulo1);
+    console.log('cerramos navegador');
+    browser.close();
+    console.log('navegador cerrado');
+})();
