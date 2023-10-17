a4289147ef2fa658174b220fc19778d539903bd0
mago_d_Mercurio <davila.feria@gmail.com>
2023-10-17 10:25:01 -0500

    add moment.js

--- paquetes/utiles/bcrypt.js
+++ paquetes/utiles/bcrypt.js
@@ -4,4 +4,9 @@ const password = '1234Segura';
 
 bcrypt.hash(password, 5, function(err, hash) {
   console.log(hash)
+
+  bcrypt.compare('password', hash, function(err, res) {
+    console.log(res)
+  
+  })
 })
